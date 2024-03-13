968949d68badfa16621f2f6c671b1d62e7d90bd9
冯燕 <124764001+fengyan0614@users.noreply.github.com>
2024-03-13 20:29:41 +0800

    Update Orificefriction.py
    将循环放入函数中，主函数可直接调用

--- Orificefriction.py
+++ Orificefriction.py
@@ -1,6 +1,14 @@
 # Orificefriction.py
+import numpy as np
+
 # 计算孔眼摩阻
-def cpf(displacement, density_fluid, quantity, pdiameter, coeff2):
-    # 该函数现在接受单个流量值，并返回计算得到的单个摩擦值
-    friction = 8.1 * density_fluid * (displacement ** 2) / ((coeff2 ** 2 )* (quantity ** 2 )* (pdiameter ** 4))
-    return friction
+def cpf(displacement_per_second, fluid_density, sand_density, sand_concentration, quantity, pdiameter, coeff2):
+    # 初始化摩阻数组
+    friction_array = np.zeros(len(displacement_per_second))
+
+    # 遍历displacement_per_second数组的每个元素
+    for i, displacement in enumerate(displacement_per_second):
+        mixed_density = (1 - sand_concentration[i]) * fluid_density + sand_concentration[i] * sand_density
+        friction_array[i] = 0.81 * mixed_density * (displacement ** 2) / ((coeff2 ** 2) * (quantity ** 2) * (pdiameter ** 4))
+
+    return friction_array
