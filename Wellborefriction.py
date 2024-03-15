3d6ff0a53521f755fcb19f07d240ffd5a743bf87
冯燕 <124764001+fengyan0614@users.noreply.github.com>
2024-03-15 09:54:52 +0800

    Update Wellborefriction.py

--- Wellborefriction.py
+++ Wellborefriction.py
@@ -1,49 +1,50 @@
 # Wellborefriction.py
+
 import numpy as np
+def ctfi(Q_fluid, Vp_proppant, Proppantmidu, fluidmidu, Z, D, R, u, detaTime=1):
+    P_well_f = np.zeros(len(Q_fluid))
+    lamdas = []
+    deta_Z = np.zeros(len(Q_fluid))
 
-def cffv(q, wdiameter):
-    return 4 * q / (np.pi * wdiameter ** 2)
+    for t in range(len(Q_fluid)):
+        # 根据雷诺数计算水力摩阻系数
+        v = 4 * Q_fluid[t] / (np.pi * D ** 2)  # 当前时刻井筒流速
+        epsilon = 2 * R / D  # 井筒壁面粗糙程度
+        Re = fluidmidu * v * D / u  # 流动雷诺数
 
-def calculate_hydraulic_friction_coefficient(q, mixed_density, wdiameter, wellbore_roughness, fluid_viscosity):
-    v = cffv(q, wdiameter)
-    epsilon = 2 * wellbore_roughness / wdiameter
-    Re = mixed_density * v * wdiameter / fluid_viscosity
+        if Re < 2000:
+            lamda = 64 / Re  # 计算水力摩阻系数
+        elif 2000 < Re < 59.7 / epsilon ** (8 / 7):
+            lamda = 0.3164 / Re ** (0.25)
+        elif 59.7 / epsilon ** (8 / 7) < Re < (665 - 765 * np.log(epsilon)) / epsilon:
+            lamda = (1 / (-1.8 * np.log(6.8 / Re + (R / (3.7 * D)) ** 3.11))) ** 2
+        elif Re > (665 - 765 * np.log(epsilon)) / epsilon:
+            lamda = 1 / (2 * np.log(3.7 * D / R)) ** 2
 
-    small_number = np.finfo(float).eps
-    Re_safe = np.maximum(Re, small_number)
-    epsilon_safe = np.maximum(epsilon, small_number)
+        lamdas.append(lamda)  # 计算的lamda值添加到lamdas数组中
+        deta_Z[t] = v * detaTime
 
-    lamda = np.where(Re_safe <= 2000, 64 / Re_safe, 0)
-    lamda = np.where((Re_safe > 2000) & (Re_safe <= (59.7 / epsilon_safe**(8/7))), 0.3164 / Re_safe**(0.25), lamda)
-    lamda = np.where((Re_safe > (59.7 / epsilon_safe**(8/7))) & (Re_safe < (665 - 765 * np.log(epsilon_safe)) / epsilon_safe),
-                    (1 / (-1.8 * np.log(6.9 / Re_safe + (epsilon_safe / 3.7)**1.11)))**2, lamda)
-    lamda = np.where(Re_safe >= ((665 - 765 * np.log(epsilon_safe)) / epsilon_safe),
-                    1 / (2 * (np.log(3.7 / epsilon_safe))**2), lamda)
+        # 清除中间信息数据
+        well_Z = []  # 更新液柱段
+        well_Vp = []  # 更新液柱段的砂比
+        well_mix_midu = []  # 更新混合液密度
 
-    return lamda
+        # 更新液柱段，液柱段的砂比，和混合液密度
+        if np.sum(deta_Z[:t+1])<Z:
+            well_Z = list(deta_Z[:t+1]) + [Z - np.sum(deta_Z[:t+1])]
+            well_Vp = list(Vp_proppant[:t+1]) + [0]
+        else:
+            sum_so_far = 0
+            boundary = 0
+            for i in range(t, -1, -1):
+                sum_so_far += deta_Z[i]
+                if sum_so_far > Z:
+                    boundary = i
+                    break
+            well_Z = list(deta_Z[i] for i in range(t, boundary, -1)) + [Z - np.sum(deta_Z[i] for i in range(t, boundary, -1))]
+            well_Vp = list(Vp_proppant[i] for i in range(t, boundary, -1)) + [Vp_proppant[boundary]]
 
-def ctfi(displacement_per_second, sand_concentration, density_sand, density_fluid, well_diameter, well_length_L, wellbore_roughness, fluid_viscosity, time_step=1):
-    # 初始化数组
-    fluid_integral = np.zeros(len(displacement_per_second))
+        well_mix_midu = [(1 - well_Vp[i]) * fluidmidu + well_Vp[i] * Proppantmidu for i in range(len(well_Z))]
+        P_well_f[t] = np.sum(np.array(well_Z) * ((lamda / D * v ** 2) * np.array(well_mix_midu) / 2))  # 井筒摩阻
 
-    for i in range(len(displacement_per_second)):
-        # 计算流速
-        flow_velocity = cffv(displacement_per_second[i], well_diameter)
-
-        # 计算水力摩阻系数
-        hfc = calculate_hydraulic_friction_coefficient(displacement_per_second[i], density_fluid, well_diameter,
-                                                       wellbore_roughness, fluid_viscosity)
-
-        # 计算混合液密度
-        mixed_density = (1 - sand_concentration[i]) * density_fluid + sand_concentration[i] * density_sand
-
-        # 计算单段压降
-        unit_pressure_drop = (hfc / well_diameter) * (flow_velocity ** 2) * (mixed_density / 2)
-
-        # 计算总压降
-        total_pressure_drop = unit_pressure_drop * well_length_L
-
-        # 存储结果
-        fluid_integral[i] = total_pressure_drop
-
-    return fluid_integral
+    return P_well_f
