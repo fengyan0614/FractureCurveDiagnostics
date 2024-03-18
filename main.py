8e62a60b8d21c632204140f4499caecf59e2ae70
冯燕 <124764001+fengyan0614@users.noreply.github.com>
2024-03-18 17:57:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 # main.py
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 from Staticpressureofliquidcolumn import cfcpif
 from Wellborefriction import ctfi
 from Orificefriction import cpf
 from NetpressureBottom import calculate_bottomhole_pressure
 from Plot import pressure_curve, plot_combined_curves
-from datetime import datetime, time
+from datetime import time
 import traceback
+from Dataprocessing import trend_analysis
+
+
 # 读取 Excel 数据
 def read_excel_data(excel_path):
     df = pd.read_excel(excel_path)
@@ -38,6 +41,9 @@ perforation_flow_coefficient = 0.95  # 孔眼流量系数
 fluid_viscosity = 0.012  # 压裂液黏度，Pa·s
 wellbore_roughness = 0.0001  # 井筒管壁绝对粗糙度，m
 perforation_quantity = 60  # 孔眼数量，个
+
+
+
 # 计算井底静压力并绘制曲线
 def calculate_bottomhole_pressure_from_excel(excel_path):
     # 井筒内携沙液柱静压力
@@ -49,6 +55,12 @@ def calculate_bottomhole_pressure_from_excel(excel_path):
     # 计算井底净压力
     bottomholepressure = calculate_bottomhole_pressure(wellhead_pressure,  fluid_integral, tubing_friction_integral, perforation_friction, min_horizontal_stress)
 
+    # 数据处理：将井底净压力的负值设为0
+    bottomholepressure[bottomholepressure < 0] = 0
+
+    # 平滑处理：对井底净压力进行移动平均滤波光滑处理
+    # 趋势分析：识别曲线的上升段和下降段，并在图形中进行标记
+    trend_analysis(time_in_seconds, bottomholepressure)
     # 得到结果列表，将其保存到文件
 
     # 创建一个pandas DataFrame
@@ -61,10 +73,9 @@ def calculate_bottomhole_pressure_from_excel(excel_path):
     })
     file_path = r"C:\Users\fy\Desktop\4-15\4-15\焦页4HF\焦页4HF压裂数据\焦页4HF井第1段压裂施工\压裂\bottomholepressure.xlsx"
     df.to_excel(file_path, index=False, engine='openpyxl')
+    # 多坐标曲线的数据及数据标签
     dataset_tuple = (tubing_friction_integral, fluid_integral, perforation_friction, wellhead_pressure, bottomholepressure)
     label_tuple = ('井筒摩阻 (Pa)', '液柱压力 (Pa)', '孔眼摩阻 (Pa)', '井口压力(Pa)', '井底净压力(Pa)')
-    # 绘制井底压力曲线
-    pressure_curve(time_in_seconds, bottomholepressure)
     # # 绘制多坐标曲线
     plot_combined_curves(time_in_seconds, dataset_tuple, label_tuple, '压力参数随时间变化图')
     # 返回所需的所有变量
@@ -77,6 +88,7 @@ if __name__ == "__main__":
     try:
         time_in_seconds, bottomholepressure, fluid_integral, tubing_friction_integral, perforation_friction = calculate_bottomhole_pressure_from_excel(excel_path)
         pressure_curve(time_in_seconds, bottomholepressure)
+
     except Exception as e:
         print(f"An error occurred: {e}")
 
